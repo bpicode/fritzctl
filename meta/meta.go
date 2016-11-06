a95eda24dc5d0528638f5220adc3a0e1b407106e
bpicode <bjoern.pirnay@googlemail.com>
2016-11-06 17:51:07 +0100

    change config file in users home to be hidden

--- meta/meta.go
+++ meta/meta.go
@@ -16,6 +16,8 @@ var (
 	Version = "unknown"
 	// ConfigFilename defines the filename of the configuration file.
 	ConfigFilename = "fritzctl.json"
+	// ConfigFilenameHidden defines the filename of the configuration file (hidden).
+	ConfigFilenameHidden = "." + ConfigFilename
 	// ConfigDir defines the directory of the configuration file.
 	ConfigDir = ""
 	// DefaultConfigDir is the default directory where the config file resides.
@@ -25,7 +27,7 @@ var (
 // ConfigFile returns the path to the config file.
 func ConfigFile() (string, error) {
 	return functional.FirstWithoutError(
-		functional.Compose(ConfigFilename, files.InHomeDir, accessible),
+		functional.Compose(ConfigFilenameHidden, files.InHomeDir, accessible),
 		functional.Curry(DefaultConfigFileAbsolute(), accessible),
 		functional.Curry(fmt.Sprintf("%s/%s", ConfigDir, ConfigFilename), accessible),
 	)
