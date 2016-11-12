66ae980a18802d27b3353f734a73ffbff8a934dd
bpicode <bjoern.pirnay@googlemail.com>
2016-11-12 19:19:32 +0100

    use subtests in checkmark test

--- console/symbols_test.go
+++ console/symbols_test.go
@@ -3,22 +3,38 @@ package console
 import (
 	"testing"
 
+	"fmt"
+
 	"github.com/stretchr/testify/assert"
 )
 
-// TestIntToCheckmark unit tests IntToCheckmark.
-func TestIntToCheckmark(t *testing.T) {
-	assert.NotEmpty(t, IntToCheckmark(0))
-	assert.NotEmpty(t, IntToCheckmark(1))
+// TestTransformToCheckmarks test the mapping of values to
+// printable symbols.
+func TestTransformToCheckmarks(t *testing.T) {
+	testData := []struct {
+		checkmark string
+	}{
+		{IntToCheckmark(0)},
+		{IntToCheckmark(1)},
+		{StringToCheckmark("")},
+		{StringToCheckmark("0")},
+		{StringToCheckmark("1")},
+	}
+	for i, testCase := range testData {
+		t.Run(fmt.Sprintf("Test checkmark %d", i), func(t *testing.T) {
+			assert.NotNil(t, testCase.checkmark)
+			assert.NotEmpty(t, testCase.checkmark)
+		})
+	}
+}
+
+// TestIntToCheckmarkDisjoint test that all target symbols are different (int version).
+func TestIntToCheckmarkDisjoint(t *testing.T) {
 	assert.NotEqual(t, IntToCheckmark(0), IntToCheckmark(1))
 }
 
-// TestStringToCheckmark unit tests StringToCheckmark.
-func TestStringToCheckmark(t *testing.T) {
-	assert.NotEmpty(t, StringToCheckmark(""))
-	assert.NotEmpty(t, StringToCheckmark("0"))
-	assert.NotEmpty(t, StringToCheckmark("1"))
-
+// TestStringToCheckmarkDisjoint test that all target symbols are different (string version).
+func TestStringToCheckmarkDisjoint(t *testing.T) {
 	assert.NotEqual(t, StringToCheckmark(""), StringToCheckmark("0"))
 	assert.NotEqual(t, StringToCheckmark("0"), StringToCheckmark("1"))
 	assert.NotEqual(t, StringToCheckmark("1"), StringToCheckmark(""))
