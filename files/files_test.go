ee0b464460ed06fd7a24ab282af78882880395ac
bpicode <bjoern.pirnay@googlemail.com>
2016-11-12 19:21:42 +0100

    improve godoc

--- files/files_test.go
+++ files/files_test.go
@@ -7,15 +7,15 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-// TestHomeDir unit test.
+// TestHomeDir tests that the running user's homedir can be resolved.
 func TestHomeDir(t *testing.T) {
 	f, err := InHomeDir("abc.txt")
 	assert.NoError(t, err)
 	assert.NotNil(t, f)
 }
 
-// TestError unit test.
-func TestError(t *testing.T) {
+// TestErrorInDirOfUser tests that the error propagation.
+func TestErrorInDirOfUser(t *testing.T) {
 	_, err := inDirOfUser("xyz.txt", nil, errors.New("some error"))
 	assert.Error(t, err)
 }
