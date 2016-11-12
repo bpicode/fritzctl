1fb8c08f6012a42945faeafd130d0acfc4669c48
bpicode <bjoern.pirnay@googlemail.com>
2016-11-12 22:32:48 +0100

    use subtests in logger tests

--- logger/logger_test.go
+++ logger/logger_test.go
@@ -3,34 +3,31 @@ package logger
 import (
 	"testing"
 
+	"fmt"
+
 	"github.com/stretchr/testify/assert"
 )
 
-// TestInfoLogging unit test.
-func TestInfoLogging(t *testing.T) {
-	assert.NotPanics(t, func() {
-		Info("some", "random", "log")
-	})
+// TestRegularLogDoNotPanic asserts that certain loggers do not panic.
+func TestRegularLogDoNotPanic(t *testing.T) {
+	loggers := []func(v ...interface{}){
+		Info,
+		Success,
+		SuccessNoTimestamp,
+		Warn,
+	}
+	for i, l := range loggers {
+		t.Run(fmt.Sprintf("Test logger %d", i), func(t *testing.T) {
+			assert.NotPanics(t, func() {
+				l("some", "random", "log")
+			})
+		})
+	}
 }
 
-// TestSuccessLogging unit test.
-func TestSuccessLogging(t *testing.T) {
-	assert.NotPanics(t, func() {
-		Success("This is a log")
-		SuccessNoTimestamp("This is another log")
-	})
-}
-
-// TestPanicLogging unit test.
+// TestPanicLogging asserts that the panic logger panics.
 func TestPanicLogging(t *testing.T) {
 	assert.Panics(t, func() {
 		Panic("I quit")
-	})
-}
-
-// TestWarnLogging should not panic.
-func TestWarnLogging(t *testing.T) {
-	assert.NotPanics(t, func() {
-		Warn("A warning")
 	})
 }
