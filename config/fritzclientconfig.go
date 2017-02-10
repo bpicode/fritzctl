a3279825c0c2fba9403dcd775d83c49007bbdeda
bpicode <bjoern.pirnay@googlemail.com>
2017-02-10 14:58:40 +0100

    fix typo

--- config/fritzclientconfig.go
+++ config/fritzclientconfig.go
@@ -31,7 +31,7 @@ type Login struct {
 
 // Pki wraps the client-side certificate handling.
 type Pki struct {
-	SkipTLSVerify   bool   `json:"skipTlsVerify"`   // Skip TLS verifcation when using https.
+	SkipTLSVerify   bool   `json:"skipTlsVerify"`   // Skip TLS verification when using https.
 	CertificateFile string `json:"certificateFile"` // Points to a certificate file (in PEM format) to verify the integrity of the FRITZ!Box.
 }
 
