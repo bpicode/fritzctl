c045a7d8a61448d8da0fae10e9df782ebc8b330d
bpicode <bjoern.pirnay@googlemail.com>
2017-04-19 19:15:22 +0200

    Refactor and fix some problems in chrono.*

--- chrono/chrono.go
+++ chrono/chrono.go
@@ -5,26 +5,34 @@ import (
 	"time"
 )
 
-// FormatEpochSecondString takes a string, parses to to an epoch second and formats it according to the following rules:
-// A simple time HH:MM:SS is displayed if the parsed date is today.
-// Day, month and time is returned if the parsed date is the current year.
-// Year, day, month and time is returned in all other cases.
+// FormatEpochSecondString takes a string, parses to to an epoch second and formats it according to FormatCompact.
 func FormatEpochSecondString(epoch string, ref time.Time) string {
-	i, err := strconv.ParseInt(epoch, 10, 64)
+	t, err := Unix(epoch)
 	if err != nil {
 		return ""
 	}
-	return FormatEpochSecondI64(i, ref)
+	return FormatCompact(t, ref)
 }
 
-// FormatEpochSecondI64 follows the same conventions as FormatEpochSecondString.
-func FormatEpochSecondI64(epoch int64, ref time.Time) string {
-	t := time.Unix(epoch, 0)
-	if ref.Day() == t.Day() {
-		return t.Format("15:04:05")
+// Unix is equivalent to time.Unix with zero nanoseconds, where the string argument passed to this function is parsed
+// as a base-10, 64-bit integer. It returns an error iff the argument could not be parsed.
+func Unix(epoch string) (time.Time, error) {
+	i, err := strconv.ParseInt(epoch, 10, 64)
+	return time.Unix(i, 0), err
+}
+
+// FormatCompact formats a given time t to a short form, given a reference time ref. It particular:
+// A simple time HH:MM:SS is displayed if t is in the same day as ref.
+// Day, month and time is returned if t is in the same year as ref.
+// Year, day, month and time is returned in all other cases.
+func FormatCompact(t, ref time.Time) string {
+	refYear, refMonth, refDay := ref.Date()
+	tYear, tMonth, tDay := t.Date()
+	if refYear != tYear {
+		return t.Format("Mon Jan 2 15:04:05 2006")
 	}
-	if ref.Year() == t.Year() {
+	if refMonth != tMonth || refDay != tDay {
 		return t.Format("Mon Jan 2 15:04:05")
 	}
-	return t.Format("Mon Jan 2 15:04:05 2006")
+	return t.Format("15:04:05")
 }
