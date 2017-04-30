4347b61fe6da9881c2f269edd312bcc5037e0495
bpicode <bjoern.pirnay@googlemail.com>
2017-04-30 21:02:37 +0200

    fmt docs

--- manifest/model.go
+++ manifest/model.go
@@ -9,11 +9,11 @@ type Plan struct {
 // Switch represents the state of a switch.
 type Switch struct {
 	Name  string // Name of the switch.
-	State bool   // On (true) or off (false)
+	State bool   // On (true) or off (false).
 }
 
 // Thermostat represents the state of a HKR device.
 type Thermostat struct {
 	Name        string  // Name of the device.
-	Temperature float64 // The temperature in °C
+	Temperature float64 // The temperature in °C.
 }
