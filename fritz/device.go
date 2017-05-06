67c111e1521aaebd8c7d0c2f939341d2fbb1e1bd
bpicode <bjoern.pirnay@googlemail.com>
2017-05-06 14:58:59 +0200

    extract device model to separate file

---
+++ fritz/device.go
@@ -0,0 +1,33 @@
+package fritz
+
+import "strings"
+
+// Device models a smart home device. This corresponds to
+// the single entries of the xml that the FRITZ!Box returns.
+// codebeat:disable[TOO_MANY_IVARS]
+type Device struct {
+	Identifier      string      `xml:"identifier,attr"`      // A unique ID like AIN, MAC address, etc.
+	ID              string      `xml:"id,attr"`              // Internal device ID of the FRITZ!Box.
+	Functionbitmask string      `xml:"functionbitmask,attr"` // Bitmask determining the functionality of the device: bit 6: Comet DECT, HKR, "thermostat", bit 7: energy measurment device, bit 8: temperature sensor, bit 9: switch, bit 10: AVM DECT repeater
+	Fwversion       string      `xml:"fwversion,attr"`       // Firmware version of the device.
+	Manufacturer    string      `xml:"manufacturer,attr"`    // Manufacturer of the device, usually set to "AVM".
+	Productname     string      `xml:"productname,attr"`     // Name of the product, empty for unknown or undefined devices.
+	Present         int         `xml:"present"`              // Device connected (1) or not (0).
+	Name            string      `xml:"name"`                 // The name of the device. Can be assigned in the web gui of the FRITZ!Box.
+	Switch          Switch      `xml:"switch"`               // Only filled with sensible data for switch devices.
+	Powermeter      Powermeter  `xml:"powermeter"`           // Only filled with sensible data for devices with an energy actuator.
+	Temperature     Temperature `xml:"temperature"`          // Only filled with sensible data for devices with a temperature sensor.
+	Thermostat      Thermostat  `xml:"hkr"`                  // Thermostat data, only filled with sensible data for HKR devices.
+}
+
+// codebeat:enable[TOO_MANY_IVARS]
+
+// IsSwitch returns true if the device is recognized to be a switch and returns false otherwise.
+func (d *Device) IsSwitch() bool {
+	return d.Powermeter.Power != "" || d.Powermeter.Energy != "" || strings.Contains(d.Productname, "FRITZ!DECT")
+}
+
+// IsThermostat returns true if the device is recognized to be a HKR device and returns false otherwise.
+func (d *Device) IsThermostat() bool {
+	return d.Thermostat.Measured != "" || d.Thermostat.Goal != "" || d.Thermostat.Saving != "" || d.Thermostat.Comfort != "" || strings.Contains(d.Productname, "Comet DECT")
+}
--- fritz/devicelist.go
+++ fritz/devicelist.go
@@ -1,32 +1,10 @@
 package fritz
 
-import "strings"
-
 // Devicelist wraps a list of devices. This corresponds to
 // the outer layer of the xml that the FRITZ!Box returns.
 type Devicelist struct {
 	Devices []Device `xml:"device"`
 }
-
-// Device models a smart home device. This corresponds to
-// the single entries of the xml that the FRITZ!Box returns.
-// codebeat:disable[TOO_MANY_IVARS]
-type Device struct {
-	Identifier      string      `xml:"identifier,attr"`      // A unique ID like AIN, MAC address, etc.
-	ID              string      `xml:"id,attr"`              // Internal device ID of the FRITZ!Box.
-	Functionbitmask string      `xml:"functionbitmask,attr"` // Bitmask determining the functionality of the device: bit 6: Comet DECT, HKR, "thermostat", bit 7: energy measurment device, bit 8: temperature sensor, bit 9: switch, bit 10: AVM DECT repeater
-	Fwversion       string      `xml:"fwversion,attr"`       // Firmware version of the device.
-	Manufacturer    string      `xml:"manufacturer,attr"`    // Manufacturer of the device, usually set to "AVM".
-	Productname     string      `xml:"productname,attr"`     // Name of the product, empty for unknown or undefined devices.
-	Present         int         `xml:"present"`              // Device connected (1) or not (0).
-	Name            string      `xml:"name"`                 // The name of the device. Can be assigned in the web gui of the FRITZ!Box.
-	Switch          Switch      `xml:"switch"`               // Only filled with sensible data for switch devices.
-	Powermeter      Powermeter  `xml:"powermeter"`           // Only filled with sensible data for devices with an energy actuator.
-	Temperature     Temperature `xml:"temperature"`          // Only filled with sensible data for devices with a temperature sensor.
-	Thermostat      Thermostat  `xml:"hkr"`                  // Thermostat data, only filled with sensible data for HKR devices.
-}
-
-// codebeat:enable[TOO_MANY_IVARS]
 
 // Switches returns the devices which satisfy IsSwitch.
 func (l *Devicelist) Switches() []Device {
@@ -39,11 +17,6 @@ func (l *Devicelist) Switches() []Device {
 	return switches
 }
 
-// IsSwitch returns true if the device is recognized to be a switch and returns false otherwise.
-func (d *Device) IsSwitch() bool {
-	return d.Powermeter.Power != "" || d.Powermeter.Energy != "" || strings.Contains(d.Productname, "FRITZ!DECT")
-}
-
 // Thermostats returns the devices which satisfy IsThermostat.
 func (l *Devicelist) Thermostats() []Device {
 	var thermostats []Device
@@ -53,9 +26,4 @@ func (l *Devicelist) Thermostats() []Device {
 		}
 	}
 	return thermostats
-}
-
-// IsThermostat returns true if the device is recognized to be a HKR device and returns false otherwise.
-func (d *Device) IsThermostat() bool {
-	return d.Thermostat.Measured != "" || d.Thermostat.Goal != "" || d.Thermostat.Saving != "" || d.Thermostat.Comfort != "" || strings.Contains(d.Productname, "Comet DECT")
 }
