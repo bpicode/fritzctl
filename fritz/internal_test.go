490d5c5209fb3b10f313a351f01dfd300cf8a9fe
bpicode <bjoern.pirnay@googlemail.com>
2017-05-07 17:36:29 +0200

    Issue #31: use internal api from mockserver in tests

--- fritz/internal_test.go
+++ fritz/internal_test.go
@@ -2,76 +2,40 @@ package fritz
 
 import (
 	"fmt"
-	"io"
-	"net/http"
-	"net/http/httptest"
 	"net/url"
-	"os"
 	"reflect"
 	"runtime"
-	"sync/atomic"
 	"testing"
 
 	"github.com/bpicode/fritzctl/fritzclient"
+	"github.com/bpicode/fritzctl/mock"
 	"github.com/stretchr/testify/assert"
 )
 
 // TestInternalFritzAPI test the FRITZ API.
 func TestInternalFritzAPI(t *testing.T) {
-
-	serverAnswering := func(answers ...string) *httptest.Server {
-		it := int32(-1)
-		server := httptest.NewUnstartedServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-			ch, err := os.Open(answers[int(atomic.AddInt32(&it, 1))%len(answers)])
-			defer ch.Close()
-			if err != nil {
-				w.WriteHeader(500)
-				w.Write([]byte(err.Error()))
-			}
-			io.Copy(w, ch)
-		}))
-		return server
-	}
-
-	client := func() *fritzclient.Client {
-		cl, err := fritzclient.New("../testdata/config_localhost_test.json")
-		if err != nil {
-			panic(err)
-		}
-		return cl
-	}
-
 	testCases := []struct {
-		client *fritzclient.Client
-		server *httptest.Server
 		dotest func(t *testing.T, internal *internalHTTP)
 	}{
-		{
-			client: client(),
-			server: serverAnswering("../testdata/examplechallenge_sid_test.xml", "../testdata/examplechallenge_sid_test.xml", "../testdata/landevices_test.json"),
-			dotest: testListLanDevices,
-		},
-		{
-			client: client(),
-			server: serverAnswering("../testdata/examplechallenge_sid_test.xml", "../testdata/examplechallenge_sid_test.xml", "../testdata/logs_test.json"),
-			dotest: testListLogs,
-		},
-		{
-			client: client(),
-			server: serverAnswering("../testdata/examplechallenge_sid_test.xml", "../testdata/examplechallenge_sid_test.xml", "../testdata/traffic_mon_answer.json"),
-			dotest: testInetStats,
-		},
+		{testListLanDevices},
+		{testListLogs},
+		{testInetStats},
 	}
 	for _, testCase := range testCases {
 		t.Run(fmt.Sprintf("Test aha api %s", runtime.FuncForPC(reflect.ValueOf(testCase.dotest).Pointer()).Name()), func(t *testing.T) {
-			testCase.server.Start()
-			defer testCase.server.Close()
-			tsurl, err := url.Parse(testCase.server.URL)
+			srv := mock.New().Start()
+			defer srv.Close()
+
+			client, err := fritzclient.New("../mock/client_config_template.json")
 			assert.NoError(t, err)
-			testCase.client.Config.Net.Protocol = tsurl.Scheme
-			testCase.client.Config.Net.Host = tsurl.Host
-			loggedIn, err := testCase.client.Login()
+			u, err := url.Parse(srv.Server.URL)
 			assert.NoError(t, err)
+			client.Config.Net.Protocol = u.Scheme
+			client.Config.Net.Host = u.Host
+
+			loggedIn, err := client.Login()
+			assert.NoError(t, err)
+
 			internal := Internal(loggedIn).(*internalHTTP)
 			assert.NotNil(t, internal)
 			testCase.dotest(t, internal)
@@ -95,7 +59,7 @@ func testListLogs(t *testing.T, internal *internalHTTP) {
 	list, err := internal.ListLogs()
 	assert.NoError(t, err)
 	assert.NotNil(t, list)
-	assert.Len(t, list.Messages, 6)
+	assert.Len(t, list.Messages, 7)
 	for _, m := range list.Messages {
 		assert.NotEmpty(t, m)
 		assert.Len(t, m, 3)
