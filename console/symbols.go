b6c8de94445ecfdc4656d10135167e20fe99f98b
bpicode <bjoern.pirnay@googlemail.com>
2017-05-07 12:45:43 +0200

    gofmt

--- console/symbols.go
+++ console/symbols.go
@@ -16,7 +16,7 @@ var (
 	// Red can be used as Sprintf, where the output it wrapped in escape characters which will render the text red in terminals.
 	Red = color.New(color.Bold, color.FgRed).SprintfFunc()
 	// Green can be used as Sprintf, where the output it wrapped in escape characters which will render the text green in terminals.
-	Green  = color.New(color.Bold, color.FgGreen).SprintfFunc()
+	Green = color.New(color.Bold, color.FgGreen).SprintfFunc()
 	// Yellow can be used as Sprintf, where the output it wrapped in escape characters which will render the text yellow in terminals.
 	Yellow = color.New(color.Bold, color.FgYellow).SprintfFunc()
 )
