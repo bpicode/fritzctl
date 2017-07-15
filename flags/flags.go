a111676aac8656657e2c68f7b2c143a971b4e783
bpicode <bjoern.pirnay@googlemail.com>
2017-07-15 22:10:21 +0200

    Issue #38: move log-level value logic to flags package

--- flags/flags.go
+++ flags/flags.go
@@ -2,8 +2,11 @@ package flags
 
 import (
 	"flag"
+	"fmt"
 	"io/ioutil"
 	"os"
+
+	"github.com/bpicode/fritzctl/logger"
 )
 
 var set *flag.FlagSet
@@ -51,4 +54,28 @@ func Args() []string {
 		args = append(args, "--help")
 	}
 	return args
+}
+
+// Loglevel represents a Value for different logging configs.
+type Loglevel struct {
+	level string
+}
+
+// Type returns a name for the value type.
+func (l *Loglevel) Type() string {
+	return "string"
+}
+
+// String converts a Loglevel to human-readable format.
+func (l *Loglevel) String() string {
+	return l.level
+}
+
+// Set configures the loglevel for the application.
+func (l *Loglevel) Set(val string) error {
+	err := logger.ConfigureLogLevel(val)
+	if err != nil {
+		return fmt.Errorf("cannot apply loglevel configuration for value '%s': %v", val, err)
+	}
+	return nil
 }
