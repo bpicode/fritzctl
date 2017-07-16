5fbde248afca35e6e97fc8c94f7bd3d2e5913b85
bpicode <bjoern.pirnay@googlemail.com>
2017-07-17 00:04:37 +0200

    CQ: rename sessionid to sessionID

--- cmd/sessionid.go
+++ cmd/sessionid.go
@@ -10,14 +10,14 @@ var sessionIDCmd = &cobra.Command{
 	Short:   "Obtain a session ID",
 	Long:    "Obtain a session ID by solving the FRITZ!Box login challenge. The session ID can be used for subsequent requests until it gets invalidated.",
 	Example: "fritzctl sessionid",
-	RunE:    sessionId,
+	RunE:    sessionID,
 }
 
 func init() {
 	RootCmd.AddCommand(sessionIDCmd)
 }
 
-func sessionId(cmd *cobra.Command, args []string) error {
+func sessionID(cmd *cobra.Command, args []string) error {
 	client := clientLogin()
 	logger.Success("Successfully obtained session ID:", client.SessionInfo.SID)
 	return nil
