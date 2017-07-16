bb0c20ec0d85fc0855862d4d60b4041d5ea88b8f
bpicode <bjoern.pirnay@googlemail.com>
2017-07-16 14:54:50 +0200

    remove unused code

--- flags/flags_test.go
+++ flags/flags_test.go
@@ -2,67 +2,10 @@ package flags
 
 import (
 	"flag"
-	"os"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
 )
-
-// TestFlagsWithEmptyArgsArray is a unit test for this package.
-func TestFlagsWithEmptyArgsArray(t *testing.T) {
-	reset()
-	Parse(os.Args[1:])
-	args := Args()
-	assert.NotNil(t, args)
-}
-
-// TestFlagsWithVersionFlag is a unit test for this package.
-func TestFlagsWithVersionFlag(t *testing.T) {
-	reset()
-	Parse([]string{"-version"})
-	args := Args()
-	assert.Contains(t, args, "--version")
-}
-
-// TestFlagsWithVFlag is a unit test for this package.
-func TestFlagsWithVFlag(t *testing.T) {
-	reset()
-	Parse([]string{"-v"})
-	args := Args()
-	assert.Contains(t, args, "--version")
-}
-
-// TestFlagsWithHelpFlag is a unit test for this package.
-func TestFlagsWithHelpFlag(t *testing.T) {
-	reset()
-	Parse([]string{"--help"})
-	args := Args()
-	assert.Contains(t, args, "--help")
-}
-
-// TestFlagsWithHFlag is a unit test for this package.
-func TestFlagsWithHFlag(t *testing.T) {
-	reset()
-	Parse([]string{"-h"})
-	args := Args()
-	assert.Contains(t, args, "--help")
-}
-
-// TestRegisterStringButValueNotPresent is a unit test for this package.
-func TestRegisterStringButValueNotPresent(t *testing.T) {
-	reset()
-	sPtr := String("myvar", "xx", "my variable, default xx")
-	Parse([]string{})
-	assert.Equal(t, "xx", *sPtr)
-}
-
-// TestRegisterStringWithValueNotPresent is a unit test for this package.
-func TestRegisterStringWithValueNotPresent(t *testing.T) {
-	reset()
-	sPtr := String("myvar", "xx", "my variable, default xx")
-	Parse([]string{"--myvar=lol"})
-	assert.Equal(t, "lol", *sPtr)
-}
 
 // TestRegisterLoglevel is a unit test for setting the loglevel via CLI flag.
 func TestRegisterLoglevel(t *testing.T) {
