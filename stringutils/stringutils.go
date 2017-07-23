2e166aaf1b48f34fa3f3fb87f0312a82143f5b3c
bpicode <bjoern.pirnay@googlemail.com>
2017-07-23 09:52:05 +0200

    remove unused code

--- stringutils/stringutils.go
+++ stringutils/stringutils.go
@@ -53,29 +53,6 @@ func Contract(m map[string]string, f func(string, string) string) []string {
 	return c
 }
 
-// StringValues extracts the values of a map[string]string and returns them
-// as a slice of strings.
-func StringValues(m map[string]string) []string {
-	values := make([]string, 0, len(m))
-	for _, value := range m {
-		values = append(values, value)
-	}
-	return values
-}
-
-// Filter takes a slice of strings and returns a slice of strings
-// consisting of those strings from the input for which the filter
-// evaluated to true.
-func Filter(strs []string, f func(string) bool) []string {
-	filtered := make([]string, 0, len(strs))
-	for _, str := range strs {
-		if f(str) {
-			filtered = append(filtered, str)
-		}
-	}
-	return filtered
-}
-
 // ErrorMessages accumulates the error messages from s lice of errors.
 func ErrorMessages(errs []error) []string {
 	msgs := make([]string, 0, len(errs))
@@ -83,23 +60,4 @@ func ErrorMessages(errs []error) []string {
 		msgs = append(msgs, err.Error())
 	}
 	return msgs
-}
-
-// AppendIfAbsent appends a string to a slice, given that the string is not
-// yet present in the slice
-func AppendIfAbsent(strings []string, s string) []string {
-	if IsAbsent(strings, s) {
-		return append(strings, s)
-	}
-	return strings
-}
-
-// IsAbsent returns true if a given string is not contained in a given slice.
-func IsAbsent(strings []string, s string) bool {
-	for _, v := range strings {
-		if v == s {
-			return false
-		}
-	}
-	return true
 }
