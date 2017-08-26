11fd5ccbc92b207957c8ecb6a408fdcca4e6a5eb
bpicode <bjoern.pirnay@googlemail.com>
2017-08-26 12:37:39 +0200

    gofmt

--- fritz/landevicelist.go
+++ fritz/landevicelist.go
@@ -9,21 +9,21 @@ type LanDevices struct {
 // NetworkElement corresponds to a single entry in LanDevices.
 // codebeat:disable[TOO_MANY_IVARS]
 type NetworkElement struct {
-	Name                 string `json:"name"`
-	IP                   string `json:"ip"`
-	Mac                  string `json:"mac"`
-	UID                  string `json:"UID"`
-	Dhcp                 string `json:"dhcp"`
-	Wlan                 string `json:"wlan"`
-	Ethernet             string `json:"ethernet"`
-	Active               string `json:"active"`
-	Wakeup               string `json:"wakeup"`
-	Deleteable           string `json:"deleteable"`
-	Source               string `json:"source"`
-	Online               string `json:"online"`
-	Speed                string `json:"speed"`
-	Guest                string `json:"guest"`
-	URL                  string `json:"url"`
+	Name       string `json:"name"`
+	IP         string `json:"ip"`
+	Mac        string `json:"mac"`
+	UID        string `json:"UID"`
+	Dhcp       string `json:"dhcp"`
+	Wlan       string `json:"wlan"`
+	Ethernet   string `json:"ethernet"`
+	Active     string `json:"active"`
+	Wakeup     string `json:"wakeup"`
+	Deleteable string `json:"deleteable"`
+	Source     string `json:"source"`
+	Online     string `json:"online"`
+	Speed      string `json:"speed"`
+	Guest      string `json:"guest"`
+	URL        string `json:"url"`
 }
 
 // codebeat:enable[TOO_MANY_IVARS]
