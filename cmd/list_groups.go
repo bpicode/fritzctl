610033e389feccdd954e47fa33b1ab5a389da894
bpicode <bjoern.pirnay@googlemail.com>
2017-09-17 16:34:11 +0200

    group listing: sorting; master device; state; temperatures;

--- cmd/list_groups.go
+++ cmd/list_groups.go
@@ -2,10 +2,12 @@ package cmd
 
 import (
 	"os"
+	"sort"
 	"strings"
 
 	"github.com/bpicode/fritzctl/console"
 	"github.com/bpicode/fritzctl/fritz"
+	"github.com/bpicode/fritzctl/stringutils"
 	"github.com/spf13/cobra"
 )
 
@@ -29,9 +31,27 @@ func listGroups(cmd *cobra.Command, args []string) error {
 	return nil
 }
 
+type byGroupName []fritz.DeviceGroup
+
+// Len returns the length of the slice.
+func (p byGroupName) Len() int {
+	return len(p)
+}
+
+// Swap exchanges elements in the slice.
+func (p byGroupName) Swap(i, j int) {
+	p[i], p[j] = p[j], p[i]
+}
+
+// Less compares group names.
+func (p byGroupName) Less(i, j int) bool {
+	return p[i].Group.Name < p[j].Group.Name
+}
+
 func printGroups(list *fritz.Devicelist) {
 	groups := list.DeviceGroups()
-	table := console.NewTable(console.Headers("NAME", "MEMBERS", "PRESENT"))
+	sort.Sort(byGroupName(groups))
+	table := console.NewTable(console.Headers("NAME", "MEMBERS", "MASTER", "PRESENT", "STATE", "TEMP (MEAS/WANT/SAV/COMF) [°C]"))
 	for _, g := range groups {
 		table.Append(groupColumns(g, list))
 	}
@@ -42,7 +62,10 @@ func groupColumns(group fritz.DeviceGroup, list *fritz.Devicelist) []string {
 	return []string{
 		group.Group.Name,
 		strings.Join(memberNames(group, list), ", "),
+		masterName(group, list),
 		console.IntToCheckmark(group.Group.Present),
+		console.StringToCheckmark(group.Group.Switch.State),
+		joinTemperatures(group.Group.Thermostat),
 	}
 }
 
@@ -53,5 +76,28 @@ func memberNames(group fritz.DeviceGroup, list *fritz.Devicelist) []string {
 			names = append(names, d.Name)
 		}
 	}
+	sort.Strings(names)
 	return names
+}
+
+func masterName(group fritz.DeviceGroup, list *fritz.Devicelist) string {
+	master, ok := list.DeviceWithID(group.Group.GroupInfo.MasterDeviceID)
+	if !ok {
+		return "(none)"
+	}
+	return master.Name
+}
+
+func joinTemperatures(th fritz.Thermostat) string {
+	return strings.Join([]string{
+		valueOrQm(th.FmtMeasuredTemperature),
+		valueOrQm(th.FmtGoalTemperature),
+		valueOrQm(th.FmtSavingTemperature),
+		valueOrQm(th.FmtComfortTemperature)},
+		"/")
+}
+
+func valueOrQm(f func() string) string {
+	yellowQm := console.Yellow("?")
+	return stringutils.DefaultIfEmpty(f(), yellowQm)
 }
