d963b97a30a1822d324083cddb25fd04bd406b04
bpicode <bjoern.pirnay@googlemail.com>
2017-09-17 12:09:42 +0200

    Issue #77: base table printing

---
+++ console/table.go
@@ -0,0 +1,117 @@
+package console
+
+import (
+	"bytes"
+	"fmt"
+	"io"
+	"strings"
+)
+
+// Table contains the data to draw a table.
+type Table struct {
+	headers []string
+	rows    [][]string
+}
+
+// Option allows to mutate the table.
+type Option func(t *Table)
+
+// NewTable creates a new table and applies the given Options to it.
+func NewTable(opts ...Option) *Table {
+	table := &Table{}
+	for _, opt := range opts {
+		opt(table)
+	}
+	return table
+}
+
+// Headers is an option to store the header texts in the table.
+func Headers(hs ...string) Option {
+	return func(t *Table) {
+		t.headers = hs
+	}
+}
+
+// Append extends the table body by one line. The length of the row should match the length of the headers as well as
+// the other rows.
+func (t *Table) Append(row []string) {
+	t.rows = append(t.rows, row)
+}
+
+// Print writes the text representation of the table to a given io.Writer.
+func (t *Table) Print(w io.Writer) {
+	t.printHeaders(w)
+	t.printRows(w)
+	t.printRuler(w)
+}
+
+func (t *Table) printRows(w io.Writer) (int64, error) {
+	buf := new(bytes.Buffer)
+	for _, r := range t.rows {
+		t.printRow(r, w)
+	}
+	return buf.WriteTo(w)
+}
+
+func (t *Table) printRow(row []string, w io.Writer) {
+	for i, c := range row {
+		width := t.widthOf(i)
+		white := strings.Repeat(" ", max(width-runeLen(c), 0))
+		fmt.Fprintf(w, "| %s%s ", c, white)
+	}
+	fmt.Fprintln(w, "|")
+}
+
+func max(x, y int) int {
+	if x > y {
+		return x
+	}
+	return y
+}
+
+func (t *Table) printHeaders(w io.Writer) (int64, error) {
+	buf := new(bytes.Buffer)
+	t.printRuler(buf)
+	t.printTitles(buf)
+	t.printRuler(buf)
+	return buf.WriteTo(w)
+}
+
+func (t *Table) printRuler(w io.Writer) (int64, error) {
+	buf := new(bytes.Buffer)
+	for i := range t.headers {
+		width := t.widthOf(i)
+		dashes := strings.Repeat("-", width+2)
+		fmt.Fprintf(buf, "+%s", dashes)
+	}
+	fmt.Fprintln(buf, "+")
+	return buf.WriteTo(w)
+}
+
+func (t *Table) printTitles(w io.Writer) {
+	for i, h := range t.headers {
+		width := t.widthOf(i)
+		leftPad := strings.Repeat(" ", (width-len(h))/2)
+		rightPad := strings.Repeat(" ", (width-len(h)+1)/2)
+		fmt.Fprintf(w, "| %s%s%s ", leftPad, h, rightPad)
+	}
+	fmt.Fprintln(w, "|")
+}
+
+func (t *Table) widthOf(i int) int {
+	width := runeLen(t.headers[i])
+	for _, row := range t.rows {
+		if runeLen(row[i]) > width {
+			width = runeLen(row[i])
+		}
+	}
+	return width
+}
+
+func runeLen(s string) int {
+	l := 0
+	for range s {
+		l++
+	}
+	return l
+}
