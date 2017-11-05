dc7e116977c69323f5a005a3ec6ecb6fd6b29e69
bpicode <bjoern.pirnay@googlemail.com>
2017-11-05 16:19:47 +0100

    fix example test

--- cmd/example_test.go
+++ cmd/example_test.go
@@ -1,12 +1,15 @@
 package cmd_test
 
 import (
+	"fmt"
+
 	"github.com/bpicode/fritzctl/cmd"
 )
 
 // RootCmd is the only exported variable. Sub-commands are unexported children of RootCmd or other unexported
 // sub-commands.
 func Example() {
-	cmd.RootCmd.Execute()
-	// output:
+	use := cmd.RootCmd.Use
+	fmt.Println(use)
+	// output: fritzctl [subcommand]
 }
