759538d5e2610509995f861b0573d4f606fba330
bpicode <bjoern.pirnay@googlemail.com>
2018-09-02 18:43:46 +0200

    Issue 166: Complete capabilities as of 2018-03-28 specification
    We commit to a `(*Device) Has(...Capabilities) bool` exported method with predefined constants. Also we complete shorthand methods of the type `(*Device) IsSwitch() bool` for consistency.
    
    Reference: https://avm.de/fileadmin/user_upload/Global/Service/Schnittstellen/AHA-HTTP-Interface.pdf sha256 c3c8d032794be0df4dfc0dc3433489015cd8b947124726db00c8cf9ba4429665

--- fritz/device_test.go
+++ fritz/device_test.go
@@ -8,28 +8,29 @@ import (
 
 // TestParsingFunctionBitMask verifies correctness of the function bit.mask interpretation.
 func TestParsingFunctionBitMask(t *testing.T) {
-	isSwitch := func(d *Device) bool { return d.IsSwitch() }
-	isThermo := func(d *Device) bool { return d.IsThermostat() }
-	canMeasT := func(d *Device) bool { return d.CanMeasureTemp() }
-	canMeasP := func(d *Device) bool { return d.CanMeasurePower() }
 	for _, tc := range []struct {
 		name   string
 		mask   string
 		fct    func(*Device) bool
 		expect bool
 	}{
-		{name: "nonsense is not switch", mask: "nonsense", fct: isSwitch, expect: false},
-		{name: "nonsense is not thermostat", mask: "nonsense", fct: isThermo, expect: false},
-		{name: "nonsense cannot measure temperature", mask: "nonsense", fct: canMeasT, expect: false},
-		{name: "nonsense cannot measure power", mask: "nonsense", fct: canMeasP, expect: false},
-		{name: "2944 is switch", mask: "2944", fct: isSwitch, expect: true},
-		{name: "2944 is not thermostat", mask: "2944", fct: isThermo, expect: false},
-		{name: "2944 can measure temperature", mask: "2944", fct: canMeasT, expect: true},
-		{name: "2944 can measure power", mask: "2944", fct: canMeasP, expect: true},
-		{name: "320 is not switch", mask: "320", fct: isSwitch, expect: false},
-		{name: "320 is thermostat", mask: "320", fct: isThermo, expect: true},
-		{name: "320 can measure temperature", mask: "320", fct: canMeasT, expect: true},
-		{name: "320 cannot measure power", mask: "320", fct: canMeasP, expect: false},
+		{name: "nonsense is not switch", mask: "nonsense", fct: (*Device).IsSwitch, expect: false},
+		{name: "nonsense is not thermostat", mask: "nonsense", fct: (*Device).IsThermostat, expect: false},
+		{name: "nonsense cannot measure temperature", mask: "nonsense", fct: (*Device).CanMeasureTemp, expect: false},
+		{name: "nonsense cannot measure power", mask: "nonsense", fct: (*Device).CanMeasurePower, expect: false},
+		{name: "2944 is switch", mask: "2944", fct: (*Device).IsSwitch, expect: true},
+		{name: "2944 is not thermostat", mask: "2944", fct: (*Device).IsThermostat, expect: false},
+		{name: "2944 can measure temperature", mask: "2944", fct: (*Device).CanMeasureTemp, expect: true},
+		{name: "2944 can measure power", mask: "2944", fct: (*Device).CanMeasurePower, expect: true},
+		{name: "320 is not switch", mask: "320", fct: (*Device).IsSwitch, expect: false},
+		{name: "320 is thermostat", mask: "320", fct: (*Device).IsThermostat, expect: true},
+		{name: "320 can measure temperature", mask: "320", fct: (*Device).CanMeasureTemp, expect: true},
+		{name: "320 cannot measure power", mask: "320", fct: (*Device).CanMeasurePower, expect: false},
+		{name: "320 cannot repeat dect", mask: "320", fct: (*Device).CanRepeatDECT, expect: false},
+		{name: "320 cannot repeat trigger alerts", mask: "320", fct: (*Device).HasAlertSensor, expect: false},
+		{name: "320 has no microphone", mask: "320", fct: (*Device).HasMicrophone, expect: false},
+		{name: "320 has no hanfun unit", mask: "320", fct: (*Device).HasHANFUNUnit, expect: false},
+		{name: "320 does not speak hanfun protocol", mask: "320", fct: (*Device).IsHANFUNCompatible, expect: false},
 	} {
 		t.Run(tc.name, func(t *testing.T) {
 			device := &Device{Functionbitmask: tc.mask}
